8403b5cfc01b3d30beff995f802f674efe1ab4d3
ryanisnan <ryanisnan@gmail.com>
2017-07-29 17:46:41 -0700

    make a successful world

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,44 @@
-mod world;
+extern crate piston_window;
+
+use piston_window::{PistonWindow, WindowSettings};
+
+pub mod world;
+
 
 fn main() {
-    let world = world::World::generate(String::from("Random world"), 4, 4);
+    let mut world = world::World::generate(String::from("Random world"), 100, 100);
     println!("World: {:#?}", world);
+
+    // Draws a new window
+    let mut window: PistonWindow = WindowSettings::new(
+        "World Window",
+        [800, 800]
+    )
+    .exit_on_esc(true)
+    .build()
+    .unwrap();
+
+    while let Some(event) = window.next() {
+        // This runs many times per second...
+        window.draw_2d(&event, |context, graphics| {
+
+            piston_window::clear([1.0; 4], graphics);
+
+            let tile_height: f64 = 10.0;
+            let tile_width: f64 = 10.0;
+
+            for (i, row) in world.data.iter().enumerate() {
+                let i = i as f64;
+                for (j, tile) in row.iter().enumerate() {
+                    let j = j as f64;
+                    piston_window::rectangle(
+                        tile.get_color(),
+                        [j * tile_width, i * tile_height, tile_height, tile_width],
+                        context.transform,
+                        graphics
+                    );
+                }
+            }
+        });
+    }
 }
--- src/world.rs
+++ src/world.rs
@@ -4,14 +4,16 @@ extern crate rand;
 #[derive(Debug)]
 pub enum Tile {
     Grass,
-    Water
+    Water,
+    Stone
 }
 
 impl Tile {
-    pub fn get_color(&self) -> (u8, u8, u8, f64) {  // r, g, b, a
+    pub fn get_color(&self) -> [f32; 4] {  // r, g, b, a
         match *self {
-            Tile::Grass => (0, 255, 0, 1.0),
-            Tile::Water => (0, 0, 255, 1.0),
+            Tile::Grass => [0.0, 1.0, 0.0, 1.0],
+            Tile::Water => [0.0, 0.0, 1.0, 1.0],
+            Tile::Stone => [0.6, 0.6, 0.6, 1.0]
         }
     }
 
@@ -19,29 +21,32 @@ impl Tile {
         match *self {
             Tile::Grass => true,
             Tile::Water => false,
+            Tile::Stone => false,
         }
     }
 }
 
 #[derive(Debug)]
 pub struct World {
-    name: String,
-    data: Vec<Vec<Tile>>,
-    height: u8,
-    width: u8
+    pub name: String,
+    pub data: Vec<Vec<Tile>>,
+    pub height: u8,
+    pub width: u8
 }
 
 impl World {
     pub fn generate(name: String, height: u8, width: u8) -> World {
         let mut data: Vec<Vec<Tile>> = Vec::new();
-        for i in (0..height) {
+        for i in 0..height {
             let mut row: Vec<Tile> = Vec::new();
-            for j in (0..width) {
+            for j in 0..width {
                 let x = rand::random::<f64>();
-                if x > 0.5 {
+                if x >= 0.25 {
                     row.push(Tile::Grass);
-                } else {
+                } else if x >= 0.05 {
                     row.push(Tile::Water);
+                } else {
+                    row.push(Tile::Stone);
                 }
             }
             data.push(row);
